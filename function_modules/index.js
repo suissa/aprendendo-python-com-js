dac99730b32faaf8e4749b649d786115d9d7c1ac
Jean Carlo Nascimento <jnascimento@gmail.com>
2024-05-17 21:34:28 -0300

    function_modules

--- function_modules/index.js
+++ function_modules/index.js
@@ -1,3 +1,6 @@
-actions = require("./actions")
+const actions = require("./actions");
 
-console.log(actions.add(2, 3))
+console.log(actions.add(2, 3));
+console.log(actions.sub(10, 5));
+console.log(actions.mul(3, 4));
+console.log(actions.div(10, 2));
--- function_modules/index.py
+++ function_modules/index.py
@@ -1,3 +1,6 @@
 from actions import *
 
 print(add(2, 3))
+print(sub(10, 5))
+print(mul(3, 4))
+print(div(10, 2))
